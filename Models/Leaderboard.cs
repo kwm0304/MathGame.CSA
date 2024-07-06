017831f49a07b96e4c701a3a8afae79e19384e8a
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-07-05 20:24:35 -0400

    added leaderboard entry

--- Models/Leaderboard.cs
+++ Models/Leaderboard.cs
@@ -2,5 +2,22 @@ namespace MathGame.CSA.Models;
 
 public class Leaderboard
 {
-    
+  public List<LeaderboardEntry> Entries { get; private set; }
+  public Leaderboard()
+  {
+    Entries = new List<LeaderboardEntry>();
+  }
+  public void AddEntry(LeaderboardEntry entry)
+  {
+    Entries.Add(entry);
+  }
+
+  public List<LeaderboardEntry> GetByHighScore()
+  {
+    return Entries.OrderByDescending(e => e.EntryScore).ToList();
+  }
+  public List<LeaderboardEntry> GetByMostRecent()
+  {
+    return Entries.OrderByDescending(e => e.EntryDate).ThenByDescending(e => e.EntryScore).ToList();
+  }
 }
